7545b84de14705f246f008444167315b5dc4db1c
Lanky2 <104015308+lankybox02@users.noreply.github.com>
2022-05-24 17:47:36 +0000

    Workspace **INITIAL TEST**

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Bundle
 Bundle: A scratch-like, simple and offline coding platform focused on community and friends!
-
-## Disclaimer
-Scratch is developed by the Lifelong Kindergarten Group at the MIT Media Lab. See http://scratch.mit.edu
-
-Scratch Offline is not affiliated or connected in any way with Scratch, The Scratch Foundation, or The Scratch Team.
--- script.js
+++ script.js
@@ -4,13 +4,18 @@ let pages = {
   },
   "#": {
     "heading": "Home",
-    "title": "Home",
+    "title": "home",
     "content": "Oh, well this page seems a bit blank! <a href='#about'>About?</a>"
   },
   "about": {
     "heading": "About Bundle",
     "title": "about",
     "content": "Bundle is a simple and offline coding platform focused on community and friends. Effortlessly snap blocks together to create your own code - then share your masterpiece to everyone!"
+  },
+  "workspace": {
+    "heading": "Workspace",
+    "title": "workspace",
+    "content": `<textarea id='code'></textarea> <button onclick="runbundle($('#code')[0].value)">Run!</button>`
   }
 }
 
@@ -29,9 +34,44 @@ function loadPage(pagename) {
 }
 
 function displayPage(title, heading, content) {
-  document.title = title;
+  document.title = title.charAt(0).toUpperCase() + title.slice(1);
   $("#title")[0].innerText = heading;
    $(".content")[0].innerHTML = content;
 }
 
 loadPage(location.hash);
+
+function runbundle(algorithm) {
+let commands = ["start", "end"];
+
+algorithm = algorithm.split("\n"); // .slice(1, -1)
+
+if (algorithm[0] != "start") {
+    throwErr();
+}else{
+    if (algorithm[algorithm.length-1] != "end") {
+        throwErr();
+    }else{
+        algorithm.slice(1, -1)
+        for (let i = 0;i < algorithm.length;i++) {
+            if (commands.includes(algorithm[i])) {
+
+            }else{
+                if (algorithm[i].startsWith("write ")) {
+                    message(algorithm[i].slice(6));
+                }else{
+                    throwErr();
+                }
+            }
+        }
+    }
+}
+}
+
+function throwErr() {
+    alert("bad command");
+}
+
+function message(x) {
+    alert(x);
+}
