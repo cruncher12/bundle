5fb0ad62d48a5ae9421fdd3dd296680fa74b6ca0
Lanky2 <104015308+lankybox02@users.noreply.github.com>
2022-05-26 12:34:19 +0000

    Stuff

--- templates.js
+++ templates.js
@@ -1,30 +1,34 @@
-// start
-// randomise 5
-// ask Guess the number between 1 and 5!
-// if {ans}={random}
-// write Congrats, you got it!
-// if {ans}!={random}
-// write Oof, you didn't get it.
-// end
+let templates = {
+"template1": `start
+randomise 5
+ask Guess the number between 1 and 5!
+if {ans}={random}
+write Congrats, you got it!
+if {ans}!={random}
+write Oof, you didn't get it.
+end`,
+"template2": `start
+randomise 100
+write I ate 
+throw random
+write  sandwhices.
+end`,
+"template3": `start
+write You smell nice 👀
+break
+loop
+end`,
+"template4": `start
+ask what's your name?
+write hello there, 
+throw ans
+write !
+break
+write you've got a nice name :)
+end`
+}
 
-// start
-// randomise 100
-// write I ate
-// throw random
-// write  sandwhices.
-// end
-
-// start
-// write You smell nice 👀
-// break
-// loop
-// end
-
-//start
-// ask what's your name?
-// write hello there,
-// throw ans
-// write !
-// break
-// write you've got a nice name :)
-// end
+function template(x) {
+$("#code").effect("shake");
+$("#code")[0].value = templates[x];
+}
